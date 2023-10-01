e334eadc979ea6af32a95c565d99669b736f337d
seoin2356 <seoin2389@icloud.com>
2023-10-02 06:03:24 +0900

    - 01 입출력과 사칙연산 해답

---
+++ baekjoonKotlin/src/main/kotlin/PoliceAndArithmetic.kt
@@ -0,0 +1,113 @@
+import java.util.Scanner
+
+class PoliceAndArithmetic {
+    val sc : Scanner = Scanner(System.`in`)
+
+    // 2557 Hello World
+    fun helloWorld(){
+
+        println("Hello World!")
+    }
+
+    // 1000 A+B
+    fun plus(){
+        var a : Int = sc.nextInt()
+        var b : Int = sc.nextInt()
+        println(a+b)
+    }
+
+    // 1001 A-B
+    fun minus(){
+        var a : Int = sc.nextInt()
+        var b : Int = sc.nextInt()
+        println(a-b)
+    }
+
+    // 10998 A×B
+    fun multiply(){
+        var a : Int = sc.nextInt()
+        var b : Int = sc.nextInt()
+        println(a*b)
+    }
+
+    // 1008 A/B
+    fun divide(){
+        var a : Double = sc.nextDouble()
+        var b : Double = sc.nextDouble()
+        println(a/b)
+    }
+
+    // 10869 사칙연산
+
+    fun addition(){
+        var a = sc.nextInt()
+        var b = sc.nextInt()
+        println(a+b)
+        println(a-b)
+        println(a*b)
+        println(a/b)
+        print(a%b)
+    }
+
+    // 10926 ??1
+    fun strPlus(){
+        var str = sc.next()
+        print("$str??!")
+    }
+
+    // 18108  1998년생인 내가 태국에서는 2541년생?
+    fun ageAddition(){
+        val age : Int = 543
+        var input = sc.nextInt()
+        print(input-age)
+    }
+
+    // 10430 나머지
+    fun remainder(){
+        var A = sc.nextInt()
+        var B = sc.nextInt()
+        var C = sc.nextInt()
+        println((A+B)%C)
+        println(((A%C) + (B%C))%C)
+        println((A*B)%C)
+        println(((A%C) * (B%C))%C)
+    }
+
+    // 2588 곱셈
+    fun multi(){
+        var a = sc.nextInt()
+        var b = sc.nextInt()
+
+        var b_1 = b%10
+        var b_10 = b%100/10
+        var b_100 = b/100
+
+        println("${b_1*a}")
+        println("${b_10*a}")
+        println("${b_100*a}")
+        println("${a*b}")
+    }
+
+    // 11382 꼬마정인
+    fun littleJ(){
+        val arr = readLine()!!.split(" ").map{it.toLong()}
+        println("${arr[0]+arr[1]+arr[2]}")
+    }
+
+    // 10171 고양이
+    fun cat(){
+        println("\\    /\\")
+        println(" )  ( ')")
+        println("(  /  )")
+        println(" \\(__)|")
+    }
+
+    fun dog(){
+        println("|\\_/|")
+        println("|q p|   /}")
+        println("( 0 )\"\"\"\\")
+        println("|\"^\"`    |")
+        println("||_/=\\\\__|")
+    }
+
+}
