d7bb437a77da6691b961a81e4305b81b7e0852da
seoin2356 <seoin2389@icloud.com>
2023-10-04 05:52:58 +0900

    - 10951 A+B-4 EOF 주의 커밋

--- baekjoonKotlin/src/main/kotlin/Main.kt
+++ baekjoonKotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 
 fun main(args: Array<String>) {
     var check = loop()
-    check.starStamp1()
+    check.addition2()
 
 
 }
--- baekjoonKotlin/src/main/kotlin/loop.kt
+++ baekjoonKotlin/src/main/kotlin/loop.kt
@@ -142,8 +142,59 @@ class loop {
     }
 
     // 2439 별찍기2
-    fun starStamp2(){}
+    fun starStamp2(){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        var count = br.readLine().toInt()
+
+        for ( i in 1..count){
+            for(j in count downTo 1){
+                bw.write(
+                    if(j<=i) "*"
+                    else " "
+                )
+            }
+            bw.write("\n")
+        }
+        bw.flush()
+        bw.close()
+    }
+
+    // 10952 A+B-5
+
+    fun addition(){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+
+        while(true){
+            var token = StringTokenizer(br.readLine())
+            var a = token.nextToken().toInt()
+            var b = token.nextToken().toInt()
+
+            if(a==0 && b==0) break
+            println("${a+b}\n")
+            }
+    }
+
+    // 10951 A+B-4
+
+    fun addition2(){
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+        var str : String?
+        while(br.readLine().also{ str = it} !=null){
+            var token = StringTokenizer(str)
+            var a = token.nextToken().toInt()
+            var b = token.nextToken().toInt()
+
+            bw.write("${a+b}\n")
+            bw.flush()
+        }
+    }
 }
+
+
 
 
 
