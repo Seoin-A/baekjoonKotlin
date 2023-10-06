ff80203d8860de4c45b8ffb0bfa3f8f341bbe668
seoin2356 <seoin2389@icloud.com>
2023-10-07 06:26:35 +0900

    - 10809 알파벳 찾기까지 커밋

--- baekjoonKotlin/src/main/kotlin/string.kt
+++ baekjoonKotlin/src/main/kotlin/string.kt
@@ -1,6 +1,8 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
-import java.lang.StringBuilder
+import java.io.OutputStreamWriter
+
 import java.util.*
 
 class string {
@@ -15,5 +17,43 @@ class string {
         println(readLine().length)
     }
 
-    //
+    // 9086 문자열
+    fun printFirstLast() = with(Scanner(System.`in`)){
+        repeat(nextLine().toInt()){
+        var str = nextLine()
+        println("${str[0]}${str[str.length-1]}")
+        }
+    }
+
+    fun printAscii() = with(BufferedReader(InputStreamReader(System.`in`))){
+        println("${readLine()[0].toInt()}")
+    }
+
+    fun printAdditions() = with(BufferedReader(InputStreamReader(System.`in`))){
+        var bw = BufferedWriter(OutputStreamWriter(System.out))
+        val x = readLine().toInt()
+        var sum = 0
+        var arr = readLine()
+
+        for(i in 0 until x) {
+            sum += arr[i].toString().toInt()
+        }
+        bw.write("${sum}")
+        bw.flush()
+        bw.close()
+    }
+
+
+    // 10809 알파벳 찾기
+    fun printSearchStr() = with(BufferedReader(InputStreamReader(System.`in`))){
+        val str = readLine()
+        var arr = IntArray(27){-1}
+
+        for ( i in str.indices){
+            if ( arr[str[i].toInt() -97] == -1)
+                arr[str[i].toInt()-97]=i
+        }
+        arr.forEach { print("${it} ") }
+        arr.forEach { print("${it} ") }
+    }
 }
