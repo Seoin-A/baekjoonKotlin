984381cbc62662c1b4e0b4b4232a7eab619291fb
seoin2356 <seoin2389@icloud.com>
2023-10-07 20:51:16 +0900

    - 2444 별찍기 - 1..3..5 => (i-1)*2+1과 같다

--- baekjoonKotlin/src/main/kotlin/Deepening.kt
+++ baekjoonKotlin/src/main/kotlin/Deepening.kt
@@ -18,4 +18,42 @@ class Deepening {
         bw.close()
     }
 
+    // 3003  킹, 퀸, 룩, 비숍, 나이트, 폰
+    fun subChess() = with (BufferedReader(InputStreamReader(System.`in`))){
+        val arr = arrayOf(1,1,2,2,2,8)
+        var res  = readLine().split(" ").map { it.toInt() }.toMutableList()
+
+        for (i in arr.indices){
+            if(arr[i] != res[i]){
+                res[i] = arr[i] - res[i]
+            }else{
+                res[i] =0
+            }
+            print("${res[i]} ")
+        }
+    }
+
+    // 2444 별찍기 -7
+
+    fun printStar() = with(BufferedReader(InputStreamReader(System.`in`))){
+        val n = readLine().toInt()
+        for(i in 1..n){
+            for( j in 0 until n-i){
+                 print(" ")
+            }
+            for( j in 0 until (i-1)*2+1){
+                print("*")
+            }
+            println()
+        }
+        for( i in n-1 downTo 1){
+            for( j in 0 until n-i){
+                print(" ")
+            }
+            for ( j in 0 until (i-1)*2+1){
+                print("*")
+            }
+            println()
+        }
+    }
 }
