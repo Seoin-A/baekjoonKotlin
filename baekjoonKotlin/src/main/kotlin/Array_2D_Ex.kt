e74f903d9a6330b1b83e59d31f2799167a92cf94
seoin2356 <seoin2389@icloud.com>
2023-10-12 05:42:09 +0900

    - 10798 세로읽기 까지 커밋

--- baekjoonKotlin/src/main/kotlin/Array_2D_Ex.kt
+++ baekjoonKotlin/src/main/kotlin/Array_2D_Ex.kt
@@ -39,5 +39,52 @@ class Array_2D_Ex {
         }
     }
 
+    /*
+    2566 printMaxArrayIndex()
+    1. 9*9 행렬 생성
+    2. 2중 for 구문 이용하여 초기화
+    3. 초기화 화면서 max 값 저장
+    4. 출력
+    */
+    fun printMaxArrayIndex() = with ( BufferedReader(InputStreamReader(System.`in`))){
+        var arr = Array(9){IntArray(9)}
+        var max = 0;
+        var (x,y) = arrayOf(0,0)
+        for( i in 0 .. 8){
+            val t = StringTokenizer(readLine()," ")
 
+            for ( j in 0 .. 8){
+                var a = t.nextToken().toInt()
+                if(a>=max) {
+                    max = a
+                    x = i+1
+                    y = j+1
+                    arr[i][j] = a
+                }
+            }
+        }
+        print("$max\n$x $y")
+    }
+
+    /*
+    10798 세로 읽기
+    1. 5*15 행렬 생성 ' '로 초기화한다.
+    2. for문을 이용하여 대입한다.
+    3. for문을 반대로 돌려서 차례대로 출력하도록 한다 이떄 -1을 continue
+    */
+
+    fun printArrayVertical() = with (BufferedReader(InputStreamReader(System.`in`))){
+        val arr = Array(5){CharArray(15){' '} }
+        for( i in 0 until 5){
+            val t = StringTokenizer(readLine())
+            arr[i] = t.nextToken().toCharArray()
+        }
+
+        for(i in 0 until 15){
+            for( j in 0 until 5){
+                   if(arr[j].getOrNull(i) == null) continue
+                print(arr[j][i])
+            }
+        }
+    }
 }
