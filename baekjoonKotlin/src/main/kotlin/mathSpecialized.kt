3cc466bd8dc1c9843bfc1c7303f6024f1cc26e5e
seoin2356 <seoin2389@icloud.com>
2023-10-17 00:25:45 +0900

    -2745 중간까지 해결

---
+++ baekjoonKotlin/src/main/kotlin/mathSpecialized.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class mathSpecialized {
+
+    /*
+    2745 진법 변환
+    1. 진법과 문자를 n과 배열 arr에 저장한다.
+    2. 각 자리의 수가 10진수보다 크면 a : 10 .. z: 35 로 대체한다
+    */
+
+    fun printBaseConversion() = with(BufferedReader(InputStreamReader(System.`in`))){
+        val arr = readLine().toCharArray()
+        val n = readLine().toInt()
+
+        var res = 0
+
+        for(i in arr.indices){
+            if(n<=10){
+                res += arr[i]-'0'
+            }else{
+
+            }
+        }
+
+
+    }
+}
